ff4d3ab7ad98a25a023ea704d9373b2ff3a60290
neerk28 <neerk@cisco.com>
2018-09-23 15:37:32 +0530

    add GET all todos and GET by id

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -20,8 +21,31 @@ app.post('/todos', (req, res) => {
     })
     todo.save().then(doc => {
         res.send(doc);
-    }, (error) => {
-        res.status(400).send(error);
-    });
+    }).catch(e => {
+        res.status(400).send(e);
+    })
 });
+
+app.get('/todos', (req, res) => {
+    Todo.find().then(docs => {
+        res.send({docs});
+    }).catch(e => {
+        res.status(400).send(e);
+    })
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send({message: 'ID not found'});
+    }
+    Todo.findById(id).then(doc => {
+        if(!doc){
+            return res.status(404).send({message: 'ID not found'});
+        }
+        res.send({doc});
+    }).catch(e => {
+        res.status(400).send(e);
+    })
+})
 
