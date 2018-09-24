45b1f47eb4138a944b062933aa2da08b9fa1f442
neerk28 <neerk@cisco.com>
2018-09-24 23:15:52 +0530

    add lodash and patch for todo

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const mongoose = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -65,3 +66,27 @@ app.delete('/todos/:id', (req, res) => {
     })
 })
 
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send({message: 'ID not found'});
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then(todo => {
+        if(!todo){
+            return res.status(404).send({message: 'ID not found'});
+        }
+        res.send({message: 'Updated', todo});
+    }).catch(e => {
+        res.status(400).send(e);
+    })
+})
