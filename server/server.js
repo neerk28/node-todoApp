b3d2d1926bc16fc89c5316775a2ce8ab9f2ff69c
neerk28 <neerk@cisco.com>
2018-09-30 13:11:37 +0530

    added user token generation

--- server/server.js
+++ server/server.js
@@ -89,4 +89,17 @@ app.patch('/todos/:id', (req, res) => {
     }).catch(e => {
         res.status(400).send(e);
     })
-})
+});
+
+//USERS - AUTHENTICATION
+app.post('/users', (req, res) =>{
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+    user.save().then(user => {
+        return user.generateAuthToken();
+    }).then(token => {
+        res.header('x-auth', token).send(user);
+    }).catch(e => {
+        res.status(400).send(e);
+    })
+});
